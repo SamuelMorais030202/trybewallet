46eecaf98aa86e1bac26c3d1c092924f64193842
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-02-08 11:23:56 -0300

    Estilizando a página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { actionUser } from '../redux/actions';
+import './Login.css';
+import logo from '../logo.png';
 
 class Login extends React.Component {
   state = {
@@ -30,28 +32,35 @@ class Login extends React.Component {
     const verification = email
       .includes('@') && email.includes('.com') && password.length > numberValidate;
     return (
-      <div>
-        <h1>Trybe Wallet</h1>
-        <input
-          type="text"
-          value={ email }
-          onChange={ this.handleChange }
-          name="email"
-          data-testid="email-input"
-        />
-        <input
-          type="password"
-          value={ password }
-          onChange={ this.handleChange }
-          name="password"
-          data-testid="password-input"
-        />
-        <button
-          onClick={ this.handleClick }
-          disabled={ !verification }
-        >
-          Entrar
-        </button>
+      <div className="main-form">
+        <div className="form">
+          <h1>
+            {' '}
+            <img src={ logo } alt="logo" />
+          </h1>
+          <input
+            type="text"
+            value={ email }
+            onChange={ this.handleChange }
+            name="email"
+            data-testid="email-input"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={ password }
+            onChange={ this.handleChange }
+            name="password"
+            data-testid="password-input"
+            placeholder="Senha"
+          />
+          <button
+            onClick={ this.handleClick }
+            disabled={ !verification }
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
